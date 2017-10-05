945501b4a19561b94ee94650d259a1077eac6199
JT9 <caleb.tsosie@asu.edu>
2017-10-05 03:21:57 -0700

    Setup Firebase and ViewController
    Setup project to use Firebase, setup the SignInViewController to with textFields and button

---
+++ SnapChatClone/SignInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignInViewController.swift
+//  SnapChatClone
+//
+//  Created by C T on 10/5/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func signInTapped(_ sender: Any) {
+        
+    }
+
+}
+

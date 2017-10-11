a6b29d5c9e8bdc1efb0df5eb485a52853d53ad99
C T <caleb.tsosie@asu.edu>
2017-10-11 00:35:11 -0700

    Created Snap Class, Added TableView to SnapsViewController
    Added a Snap class for use in the SnapsViewController.  Also added a
    TableView to SnapsViewController in order to display snaps that were
    left by users

---
+++ SnapChatClone/Models/Snap.swift
@@ -0,0 +1,15 @@
+//
+//  Snap.swift
+//  SnapChatClone
+//
+//  Created by Caleb Tsosie on 10/11/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import Foundation
+
+class Snap {
+    var imageURl = ""
+    var descrip = ""
+    var from = ""
+}
--- SnapChatClone/ViewControllers/SnapsViewController.swift
+++ SnapChatClone/ViewControllers/SnapsViewController.swift
@@ -7,15 +7,53 @@
 //
 
 import UIKit
+import Firebase
+class SnapsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-class SnapsViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var snaps: [Snap] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //setup TableView
+        tableView.dataSource = self
+        tableView.delegate = self
 
-        // Do any additional setup after loading the view.
+        //Load users from Firebase Database
+        Database.database().reference().child("users").child(Auth.auth().currentUser!.uid).child("snaps").observe(DataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+            
+            
+            //From User.swift
+            let snap = Snap()
+            let snapshotValue = snapshot.value as? NSDictionary
+            snap.imageURl = (snapshotValue!["imageURL"] as? String)!
+            snap.from = (snapshotValue!["email"] as? String)!
+            snap.descrip = (snapshotValue!["description"] as? String)!
+            
+            //Added to snaps array
+            self.snaps.append(snap)
+            self.tableView.reloadData()
+         
+        })
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snaps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let snap = snaps[indexPath.row]
+        
+        cell.textLabel?.text = snap.from
+        
+        return cell
+    }
 
     @IBAction func logoutTapped(_ sender: Any) {
         
